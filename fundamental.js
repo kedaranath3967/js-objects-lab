0bf0679a2564d68b0afaf4307cf20e501f79143b
Kedaranath Ambekar <72230613+kedaranath3967@users.noreply.github.com>
2022-09-20 00:24:12 +0100

    Arrays.

--- fundamental.js
+++ fundamental.js
@@ -1,5 +1,9 @@
 const aCar = {
     owner : "Joe Bloggs",
+    previous_owners: [
+        { name : "Pat Smith" , address :  "1 Main Street"},
+        { name :"Sheila Dwyer" , address: "2 High Street"}
+    ],
     type: {
     make : 'Toyota',
     model : 'Corolla',
@@ -7,8 +11,20 @@ const aCar = {
     },
     registration : {year:201, country:"WD", number:1058}
   };
+  aCar.mileage =10000,
+
+  aCar.color     ={
+    exterior:'red',
+    interior:{
+    texture:'leather',
+    shade:'cream'}
+
+  }
   
   console.log(aCar.owner  + ' drives a ' + aCar.type.make);
   console.log("Reg.= "+aCar.registration.year+
                  "-" + aCar.registration.country+
-                 "-" + aCar.registration.number)
+                 "-" + aCar.registration.number);
+  console.log("It is " + aCar.color.exterior + " car," + aCar.mileage + " milage, with " + aCar.color.interior.texture +" interior" )
+  console.log('First owner : ' + aCar.previous_owners[0].name + '=' + aCar.previous_owners[0].address)
+            
